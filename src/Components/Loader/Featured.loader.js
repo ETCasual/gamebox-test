9854d98d675c08b80840c7749cad8168b0a2cfed
KNLim36 <47783665+KNLim36@users.noreply.github.com>
2022-02-18 11:31:46 +0800

    - Updated content loader size for featured prize

--- src/Components/Loader/Featured.loader.js
+++ src/Components/Loader/Featured.loader.js
@@ -2,17 +2,17 @@ import React from "react";
 import ContentLoader from "react-content-loader";
 
 const FeaturedLoader = (props) => (
-    <div className="col-12 col-md-10 col-lg-8 col-xl-8 mx-auto mb-4">
+    <div className="col-12 col-md-10 col-lg-8 col-xl-7 mx-auto mb-4">
         <ContentLoader
-            style={{ width: "100%", minHeight: "75vh" }}
+            style={{ width: "100%", minHeight: "70vh", height:"70vh" }}
             speed={2}
             backgroundColor="#d4d4d4"
             foregroundColor="#ecebeb"
             opacity="0.7"
             {...props}
         >
-            <rect x="0" y="0" rx="17" ry="17" width="100%" height="87%" />
-            <rect x="0" y="88%" rx="17" ry="17" width="100%" height="10.5%" />
+            <rect x="0" y="0" rx="17" ry="17" width="100%" height="100%" />
+            {/* <rect x="0" y="88%" rx="17" ry="17" width="100%" height="10.5%" /> */}
         </ContentLoader>
     </div>
 );
