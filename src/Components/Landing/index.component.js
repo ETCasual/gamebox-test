a67f20e2a9e7c4a179286a47ee08bd095553511d
Rohail <rohail384@gmail.com>
2022-03-10 11:36:52 +0800

    landing page ref null bug fixed

--- src/Components/Landing/index.component.js
+++ src/Components/Landing/index.component.js
@@ -34,77 +34,85 @@ const Index = () => {
 
     // SCROLL TRIGGER ANIMATIONS
     useEffect(() => {
-        // HERO
-        const g1 = ScrollTrigger.create({
-            trigger: heroRef.current,
-            start: "top bottom",
-            onEnter: () => {
-                const tl = gsap.timeline();
-                tl.to(
-                    heroRef.current,
-                    {
-                        duration: 1,
-                        autoAlpha: 1,
-                        y: 0,
-                        ease: "power4.out",
-                    },
-                    0
-                ).to(
-                    [
-                        workRef.current.childNodes[0],
-                        workRef.current.childNodes[1],
-                    ],
-                    {
-                        duration: 0.6,
-                        autoAlpha: 1,
-                        y: 0,
-                        ease: "power4.out",
-                        stagger: 0.2,
-                    },
-                    0
-                );
-            },
-        });
+        let g1, g2, g3;
+        if (
+            heroRef.current &&
+            workRef.current &&
+            workCardRef.current &&
+            dailyRewardRef.current
+        ) {
+            // HERO
+            g1 = ScrollTrigger.create({
+                trigger: heroRef.current,
+                start: "top bottom",
+                onEnter: () => {
+                    const tl = gsap.timeline();
+                    tl.to(
+                        heroRef.current,
+                        {
+                            duration: 1,
+                            autoAlpha: 1,
+                            y: 0,
+                            ease: "power4.out",
+                        },
+                        0
+                    ).to(
+                        [
+                            workRef.current.childNodes[0],
+                            workRef.current.childNodes[1],
+                        ],
+                        {
+                            duration: 0.6,
+                            autoAlpha: 1,
+                            y: 0,
+                            ease: "power4.out",
+                            stagger: 0.2,
+                        },
+                        0
+                    );
+                },
+            });
 
-        // WORK
-        const g2 = ScrollTrigger.create({
-            trigger: workCardRef.current,
-            start: "10% bottom",
-            onEnter: () => {
-                if (workCardRef.current !== null && gsap)
-                    gsap.to(workCardRef.current, {
-                        duration: 1,
-                        autoAlpha: 1,
-                        y: 0,
-                        ease: "power2.out",
-                        stagger: 0.2,
-                    });
-            },
-        });
+            // WORK
+            g2 = ScrollTrigger.create({
+                trigger: workCardRef.current,
+                start: "10% bottom",
+                onEnter: () => {
+                    if (workCardRef.current !== null && gsap)
+                        gsap.to(workCardRef.current, {
+                            duration: 1,
+                            autoAlpha: 1,
+                            y: 0,
+                            ease: "power2.out",
+                            stagger: 0.2,
+                        });
+                },
+            });
 
-        // DAILY REWARD
-        const g3 = ScrollTrigger.create({
-            trigger: dailyRewardRef.current,
-            start: "100px bottom",
-            onEnter: () => {
-                const tl = gsap.timeline();
-                tl.to(
-                    [
-                        dailyRewardRef.current.childNodes[0],
-                        dailyRewardRef.current.childNodes[1],
-                        dailyRewardRef.current.childNodes[2],
-                    ],
-                    {
-                        duration: 0.6,
-                        autoAlpha: 1,
-                        y: 0,
-                        ease: "power4.out",
-                        stagger: 0.2,
-                    },
-                    0
-                );
-            },
-        });
+            // DAILY REWARD
+            g3 = ScrollTrigger.create({
+                trigger: dailyRewardRef.current,
+                start: "100px bottom",
+                onEnter: () => {
+                    const tl = gsap.timeline();
+                    tl.to(
+                        [
+                            dailyRewardRef.current.childNodes[0],
+                            dailyRewardRef.current.childNodes[1],
+                            dailyRewardRef.current.childNodes[2],
+                        ],
+                        {
+                            duration: 0.6,
+                            autoAlpha: 1,
+                            y: 0,
+                            ease: "power4.out",
+                            stagger: 0.2,
+                        },
+                        0
+                    );
+                },
+            });
+        }
 
         return () => {
             g1.kill();
