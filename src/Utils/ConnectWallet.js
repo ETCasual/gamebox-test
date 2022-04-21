789b423d1cde6bf2165f53cbf1ec5274c4e8b4b0
PhilipAppxplore <philipAppxplore@gmail.com>
2022-04-21 15:49:01 +0800

    Move Meta Mask toast error message to connectWallet.js

--- src/Utils/ConnectWallet.js
+++ src/Utils/ConnectWallet.js
@@ -5,11 +5,10 @@ import {
 import Web3 from "web3";
 import tokenABI from "./TokenABI";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+import { SHOW_TOAST } from "redux/types";
 import _ from "lodash";
-
 // Check if any wallet is connected then connect
 export async function handleConnectWallet(dispatch) {
-    
     const { web3, provider } = await getWeb3();
     if (!web3) return;
     console.log(web3);
@@ -22,7 +21,6 @@ export async function handleConnectWallet(dispatch) {
         (n) => n.chainId === parseInt(chainId)
     );
     try {
-
         if (isIncluded === -1) {
             if (accounts.length > 0) {
                 dispatch(
@@ -34,7 +32,10 @@ export async function handleConnectWallet(dispatch) {
                     )
                 );
             }
-            const error = { code: 4903, message: "Connected network not supported" };
+            const error = {
+                code: 4903,
+                message: "Connected network not supported",
+            };
             throw error;
         } else {
             // IF WALLET ADDRESS IS AVAILABLE
@@ -60,18 +61,18 @@ export async function handleConnectWallet(dispatch) {
         console.log(error);
         if (error.code === -32002 || error.code === 4001)
             dispatch(loadConnectWalletAutoError(error));
-        if (error.code === 4903) {    
-            const networks = JSON.parse(sessionStorage.getItem("networks")) || [];
+        if (error.code === 4903) {
+            const networks =
+                JSON.parse(sessionStorage.getItem("networks")) || [];
 
             try {
                 // check if the chain to connect to is installed
                 await web3.currentProvider.request({
-                    method: 'wallet_switchEthereumChain',
-                    params: [{ chainId: web3.utils.toHex(
-                        networks[0].chainId
-                    ) }], // chainId must be in hexadecimal numbers
+                    method: "wallet_switchEthereumChain",
+                    params: [
+                        { chainId: web3.utils.toHex(networks[0].chainId) },
+                    ], // chainId must be in hexadecimal numbers
                 });
-
             } catch (error) {
                 // Already trigger, check metamask
                 if (error.code === -32002) {
@@ -85,19 +86,19 @@ export async function handleConnectWallet(dispatch) {
                         )
                     );
 
-                // This error code indicates that the chain has not been added to MetaMask
-                // if it is not, then install it into the user MetaMask
+                    // This error code indicates that the chain has not been added to MetaMask
+                    // if it is not, then install it into the user MetaMask
                 } else if (error.code === 4902) {
                     try {
                         await web3.currentProvider.request({
-                            method: 'wallet_addEthereumChain',
+                            method: "wallet_addEthereumChain",
                             params: [
-                            {
-                                chainId: web3.utils.toHex(
-                                    networks[0].chainId
-                                ),
-                                rpcUrl: networks[0].rpcUrl,
-                            },
+                                {
+                                    chainId: web3.utils.toHex(
+                                        networks[0].chainId
+                                    ),
+                                    rpcUrl: networks[0].rpcUrl,
+                                },
                             ],
                         });
                     } catch (addError) {
@@ -107,12 +108,11 @@ export async function handleConnectWallet(dispatch) {
                     console.error(error);
                 }
             }
-
         }
     }
 
-    const wallet = localStorage.getItem('wallet');
-    if (wallet === 'Metamask') {
+    const wallet = localStorage.getItem("wallet");
+    if (wallet === "Metamask") {
         window.ethereum.on("accountsChanged", handleAccountChanged);
         window.ethereum.on("chainChanged", (chainId) => {
             if (chainId) window.location.reload();
@@ -120,29 +120,18 @@ export async function handleConnectWallet(dispatch) {
         window.ethereum.on("disconnect", (err) => {
             // localStorage.removeItem('wallet');
             dispatch(
-                loadConnectUserWallet(
-                    "wallet_disconnected",
-                    null,
-                    null,
-                    null
-                )
+                loadConnectUserWallet("wallet_disconnected", null, null, null)
             );
-        })
-    
-    } else if (wallet === 'WalletConnect') {
+        });
+    } else if (wallet === "WalletConnect") {
         provider.on("accountsChanged", handleAccountChanged);
         provider.on("chainChanged", (chainId) => {
             if (chainId) window.location.reload();
         });
-        provider.on('disconnect', (code, reason) => {
+        provider.on("disconnect", (code, reason) => {
             // localStorage.removeItem('wallet');
             dispatch(
-                loadConnectUserWallet(
-                    "wallet_disconnected",
-                    null,
-                    null,
-                    null
-                )
+                loadConnectUserWallet("wallet_disconnected", null, null, null)
             );
         });
     }
@@ -170,25 +159,23 @@ export async function handleConnectWallet(dispatch) {
                 );
         }
     }
-
 }
 
 export async function getWeb3() {
-    const wallet = localStorage.getItem('wallet');
+    const wallet = localStorage.getItem("wallet");
 
     let web3;
     let provider;
-    if (wallet === 'Metamask') {
+    if (wallet === "Metamask") {
         web3 = new Web3(window.ethereum);
-    } else if (wallet === 'WalletConnect') {
-
+    } else if (wallet === "WalletConnect") {
         const networks = JSON.parse(sessionStorage.getItem("networks")) || [];
         const defChainId = networks[0]?.chainId;
         const rpcUrls = {};
         _.forEach(networks, (net) => {
             _.set(rpcUrls, net.chainId, net.rpcUrl);
-        })
-    
+        });
+
         provider = new WalletConnectProvider({
             rpc: rpcUrls,
             chainId: defChainId,
@@ -198,16 +185,16 @@ export async function getWeb3() {
             await provider.enable();
             web3 = new Web3(provider);
         } catch (err) {
-            localStorage.removeItem('wallet');
+            localStorage.removeItem("wallet");
             throw err;
         }
     }
-    return {web3, provider};
+    return { web3, provider };
 }
 
 // GET TOKEN BALANCE FROM CONTRACT ADDRESS
 export async function getTokenBalance(address) {
-    const {web3} = await getWeb3();
+    const { web3 } = await getWeb3();
 
     const chainId = await web3.eth.getChainId();
 
@@ -215,7 +202,6 @@ export async function getTokenBalance(address) {
     const idx = networks.findIndex((n) => n.chainId === parseInt(chainId));
 
     if (networks[idx]?.systemTokenAddress) {
-
         // IF CORRECT NETWORK ID THEN CONNECT TO CONTRACT
         const tokenContract = new web3.eth.Contract(
             tokenABI,
@@ -227,29 +213,39 @@ export async function getTokenBalance(address) {
         );
         const symbol = await tokenContract.methods.symbol().call();
         return { tokenBalance, symbol };
-    } 
+    }
     return {};
 }
 
-export async function handleMetamask() {
-    localStorage.setItem('wallet', 'Metamask');
-    await window.ethereum.enable();
+export async function handleMetamask(dispatch) {
+    if (!window.web3) {
+        dispatch({
+            type: SHOW_TOAST,
+            payload: {
+                message: "Please install Metamask extension !",
+            },
+        });
+        throw "Metamask extension not installed";
+    } else {
+        localStorage.setItem("wallet", "Metamask");
+        await window.ethereum.enable();
+    }
 }
 
 export async function handleWalletConnect() {
-    localStorage.setItem('wallet', 'WalletConnect');
+    localStorage.setItem("wallet", "WalletConnect");
 
     //  Create WalletConnect Provider
     await getWeb3();
 }
 
 export async function disconnectWallet() {
-    const {web3} = await getWeb3();
+    const { web3 } = await getWeb3();
 
-    if (typeof web3.currentProvider.disconnect === 'function') {
+    if (typeof web3.currentProvider.disconnect === "function") {
         await web3.currentProvider.disconnect();
-        localStorage.removeItem('walletconnect');
+        localStorage.removeItem("walletconnect");
     }
 
-    localStorage.removeItem('wallet');
+    localStorage.removeItem("wallet");
 }
