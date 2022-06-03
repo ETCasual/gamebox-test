08077611f288a3746343bb764432a94ab1810a33
Ong <yernyee@appxplore.com>
2022-06-03 12:07:18 +0800

    Change GA to use envvar instead of hardcode

--- src/Components/Global/GoogleAnalytics.component.js
+++ src/Components/Global/GoogleAnalytics.component.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from 'react-ga4';
 
-const TRACKING_ID = "G-5PK1M5GPM6";
+const TRACKING_ID = process.env.REACT_APP_ANALYTICS_ID;
 ReactGA.initialize(TRACKING_ID);
 
 export default function GoogleAnalytics() {
