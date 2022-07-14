6885c7076888d48250d07eeb71640004c40603a6
Hann <hann@froyo.games>
2022-07-14 08:50:07 +0800

    Update Winner Announcement modal.
    -refactoring winner annoucement modal.

--- src/Components/Modals/WinnerAnnouncementModal.component.js
+++ src/Components/Modals/WinnerAnnouncementModal.component.js
@@ -38,7 +38,8 @@ const WinnerAnnouncementModal = ({ data, user, handleBackButton }) => {
         sessionStorage.setItem("showAnnouncement", 0);
 
         let timeOutRef = null;
-        if (data.length > 0) {
+        if (data.length > 0)
+        {
             clearTimeout(timeOutRef);
             timeOutRef = setTimeout(() => {
                 setPrizeData(data);
@@ -69,168 +70,183 @@ const WinnerAnnouncementModal = ({ data, user, handleBackButton }) => {
     };
 
     return (
-        <div className="winner-announcement">
-            <Swiper
-                className="swiper"
-                spaceBetween={0}
-                pagination={{
-                    el: ".custom-pagination",
-                    clickable: true,
-                    dynamicBullets: true,
-                    renderBullet: (index, className) => {
-                        // TODO: USE STATIC IMG IN SMALL THUMBNAIL
-                        return `<div class="${className} d-flex align-items-center justify-content-center">
-                            <img src="${prizeData[index]?.picture}" alt="prize"/>
-                        </div>`;
-                    },
-                }}
-                navigation={{
-                    prevEl: navigationPrevRef.current,
-                    nextEl: navigationNextRef.current,
-                }}
-                preloadImages={true}
-                threshold={30}
-                initialSlide={1}
-                slidesPerView={1}
-                onActiveIndexChange={(active) => {
-                    setActiveSlide(active.realIndex);
-                }}
-            >
-                {prizeData.map((e, idx) => (
-                    <SwiperSlide key={idx}>
-                        <div className="prize">
-                            {/* PRIZE INFO */}
-                            <div className="picture-wrapper d-flex flex-column align-items-center text-center">
-                                {/* THUMBNAIL MEDIA */}
-                                <ThumbnailMedia
-                                    url={e.picture}
-                                    isPlayVideo={true}
-                                    className="thumb-media"
-                                />
-                                <img
-                                    width={28}
-                                    className="close-btn"
-                                    onClick={() =>
-                                        handleBackButton(e?.prizeId, e?.id)
-                                    }
-                                    src={`${window.cdn}buttons/button_close.png`}
-                                    alt="close-btn"
-                                />
-                            </div>
-                            {/* WINNER INFO */}
-                            <div className="w-100 p-4 text-center winner-wrapper">
-                                <img
-                                    className="winner-pic"
-                                    src={e?.winnerAvatarUrl}
-                                    onError={(e) => defaultUserImage(e)}
-                                    alt={e.winner}
-                                />
-                                {/* TODO: To change the checking to not use username */}
-                                {user.username.toLowerCase() ===
-                                    e.winner.toLowerCase() && (
-                                    <>
-                                        <p className="winner-name p-3">
-                                            {e.winner} (You)
-                                        </p>
-                                        <p className="won-text mb-0">Won the</p>
-                                        <p className="prize-name my-2">
-                                            {e.title}
-                                        </p>
-                                        <p className="nft-token">
-                                            TokenID:{" "}
-                                            {e.nftContractAddress.substring(
-                                                0,
-                                                5
-                                            )}
-                                            ....
-                                            {e.nftContractAddress.substring(
-                                                e.nftContractAddress.length - 4
-                                            )}
-                                        </p>
-
-                                        {e.canClaimDate > 0 && (
-                                            <>
-                                                <p className="mb-2 mt-4 not-minted">
-                                                    This NFT is not minted yet.
-                                                    We’ll notify you once it’s
-                                                    out.
-                                                </p>
-                                                <p className="mint-date">
-                                                    NFT mint date:{" "}
-                                                    {getMintDate(
-                                                        e.canClaimDate
-                                                    )}
-                                                </p>
-                                            </>
-                                        )}
-
-                                        {e.canClaimDate <= 0 && (
-                                            <Link to="/profile/rewards">
-                                                <button className="connect-wallet-btn p-3">
-                                                    Claim prize
-                                                </button>
-                                            </Link>
-                                        )}
-                                    </>
-                                )}
-                                {user.username.toLowerCase() !==
-                                    e.winner.toLowerCase() && (
-                                    <>
-                                        <p className="winner-name mt-5 p-3">
-                                            {e.winner}
-                                        </p>
-                                        <p className="won-text mb-0">won the</p>
-                                        <p className="prize-name my-2">
-                                            {e.title}
-                                        </p>
-                                        <p className="nft-token">
-                                            TokenID:{" "}
-                                            {e.nftContractAddress.substring(
-                                                0,
-                                                5
-                                            )}
-                                            ....
-                                            {e.nftContractAddress.substring(
-                                                e.nftContractAddress.length - 4
-                                            )}
-                                        </p>
-                                    </>
-                                )}
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-
-                <div
-                    className={`pagination p-3 mx-auto ${
-                        prizeData.length > 1 ? "d-flex" : "d-none"
-                    } align-items-center justify-content-between`}
-                >
-                    <img
-                        width={32}
-                        className={`prev ${
-                            activeSlide > 0 ? "" : "opacity-0-5"
-                        }`}
-                        ref={navigationPrevRef}
-                        src={`${window.cdn}buttons/button_back.png`}
-                        alt="prev-btn"
-                    />
-                    <div className="custom-pagination d-flex justify-content-center"></div>
-                    <img
-                        width={32}
-                        className={`next ${
-                            activeSlide === prizeData.length - 1
-                                ? "opacity-0-5"
-                                : ""
-                        }`}
-                        ref={navigationNextRef}
-                        src={`${window.cdn}buttons/button_back.png`}
-                        alt="next-btn"
-                    />
-                </div>
-            </Swiper>
+        <div className="container-fluid d-flex align-items-center justify-content-center modal-pop">
+            <div className="modal-body-winner-announcement position-relative">
+                {/* CLOSE BTN */}
+                <img
+                    className="close-button"
+                    onClick={handleBackButton}
+                    width="36"
+                    src={`${window.cdn}buttons/button_close.png`}
+                    alt="close-btn"
+                />
+            </div>
         </div>
     );
+
+    // return (
+    //     <div className="winner-announcement">
+    //         <Swiper
+    //             className="swiper"
+    //             spaceBetween={0}
+    //             pagination={{
+    //                 el: ".custom-pagination",
+    //                 clickable: true,
+    //                 dynamicBullets: true,
+    //                 renderBullet: (index, className) => {
+    //                     // TODO: USE STATIC IMG IN SMALL THUMBNAIL
+    //                     return `<div class="${className} d-flex align-items-center justify-content-center">
+    //                         <img src="${prizeData[index]?.picture}" alt="prize"/>
+    //                     </div>`;
+    //                 },
+    //             }}
+    //             navigation={{
+    //                 prevEl: navigationPrevRef.current,
+    //                 nextEl: navigationNextRef.current,
+    //             }}
+    //             preloadImages={true}
+    //             threshold={30}
+    //             initialSlide={1}
+    //             slidesPerView={1}
+    //             onActiveIndexChange={(active) => {
+    //                 setActiveSlide(active.realIndex);
+    //             }}
+    //         >
+    //             {prizeData.map((e, idx) => (
+    //                 <SwiperSlide key={idx}>
+    //                     <div className="prize">
+    //                         {/* PRIZE INFO */}
+    //                         <div className="picture-wrapper d-flex flex-column align-items-center text-center">
+    //                             {/* THUMBNAIL MEDIA */}
+    //                             <ThumbnailMedia
+    //                                 url={e.picture}
+    //                                 isPlayVideo={true}
+    //                                 className="thumb-media"
+    //                             />
+    //                             <img
+    //                                 width={28}
+    //                                 className="close-btn"
+    //                                 onClick={() =>
+    //                                     handleBackButton(e?.prizeId, e?.id)
+    //                                 }
+    //                                 src={`${window.cdn}buttons/button_close.png`}
+    //                                 alt="close-btn"
+    //                             />
+    //                         </div>
+    //                         {/* WINNER INFO */}
+    //                         <div className="w-100 p-4 text-center winner-wrapper">
+    //                             <img
+    //                                 className="winner-pic"
+    //                                 src={e?.winnerAvatarUrl}
+    //                                 onError={(e) => defaultUserImage(e)}
+    //                                 alt={e.winner}
+    //                             />
+    //                             {/* TODO: To change the checking to not use username */}
+    //                             {user.username.toLowerCase() ===
+    //                                 e.winner.toLowerCase() && (
+    //                                 <>
+    //                                     <p className="winner-name p-3">
+    //                                         {e.winner} (You)
+    //                                     </p>
+    //                                     <p className="won-text mb-0">Won the</p>
+    //                                     <p className="prize-name my-2">
+    //                                         {e.title}
+    //                                     </p>
+    //                                     <p className="nft-token">
+    //                                         TokenID:{" "}
+    //                                         {e.nftContractAddress.substring(
+    //                                             0,
+    //                                             5
+    //                                         )}
+    //                                         ....
+    //                                         {e.nftContractAddress.substring(
+    //                                             e.nftContractAddress.length - 4
+    //                                         )}
+    //                                     </p>
+
+    //                                     {e.canClaimDate > 0 && (
+    //                                         <>
+    //                                             <p className="mb-2 mt-4 not-minted">
+    //                                                 This NFT is not minted yet.
+    //                                                 We’ll notify you once it’s
+    //                                                 out.
+    //                                             </p>
+    //                                             <p className="mint-date">
+    //                                                 NFT mint date:{" "}
+    //                                                 {getMintDate(
+    //                                                     e.canClaimDate
+    //                                                 )}
+    //                                             </p>
+    //                                         </>
+    //                                     )}
+
+    //                                     {e.canClaimDate <= 0 && (
+    //                                         <Link to="/profile/rewards">
+    //                                             <button className="connect-wallet-btn p-3">
+    //                                                 Claim prize
+    //                                             </button>
+    //                                         </Link>
+    //                                     )}
+    //                                 </>
+    //                             )}
+    //                             {user.username.toLowerCase() !==
+    //                                 e.winner.toLowerCase() && (
+    //                                 <>
+    //                                     <p className="winner-name mt-5 p-3">
+    //                                         {e.winner}
+    //                                     </p>
+    //                                     <p className="won-text mb-0">won the</p>
+    //                                     <p className="prize-name my-2">
+    //                                         {e.title}
+    //                                     </p>
+    //                                     <p className="nft-token">
+    //                                         TokenID:{" "}
+    //                                         {e.nftContractAddress.substring(
+    //                                             0,
+    //                                             5
+    //                                         )}
+    //                                         ....
+    //                                         {e.nftContractAddress.substring(
+    //                                             e.nftContractAddress.length - 4
+    //                                         )}
+    //                                     </p>
+    //                                 </>
+    //                             )}
+    //                         </div>
+    //                     </div>
+    //                 </SwiperSlide>
+    //             ))}
+
+    //             <div
+    //                 className={`pagination p-3 mx-auto ${
+    //                     prizeData.length > 1 ? "d-flex" : "d-none"
+    //                 } align-items-center justify-content-between`}
+    //             >
+    //                 <img
+    //                     width={32}
+    //                     className={`prev ${
+    //                         activeSlide > 0 ? "" : "opacity-0-5"
+    //                     }`}
+    //                     ref={navigationPrevRef}
+    //                     src={`${window.cdn}buttons/button_back.png`}
+    //                     alt="prev-btn"
+    //                 />
+    //                 <div className="custom-pagination d-flex justify-content-center"></div>
+    //                 <img
+    //                     width={32}
+    //                     className={`next ${
+    //                         activeSlide === prizeData.length - 1
+    //                             ? "opacity-0-5"
+    //                             : ""
+    //                     }`}
+    //                     ref={navigationNextRef}
+    //                     src={`${window.cdn}buttons/button_back.png`}
+    //                     alt="next-btn"
+    //                 />
+    //             </div>
+    //         </Swiper>
+    //     </div>
+    // );
 };
 
 export default WinnerAnnouncementModal;
