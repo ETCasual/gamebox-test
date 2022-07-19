432d4c3cad1988c0b79ec11a40caf33fac6659aa
lianjianhao <73788847+lianjianhao@users.noreply.github.com>
2022-07-19 17:52:41 +0800

    Fix all leaderboard duplicated
    It only happen when there is no internet connection. When there is no internet connection, data in the leaderboard cannot be loaded from backend, so it will take whatever inside the cache and display on the leaderboard.
    My solution for this is to clear the leaderboard when dispatch. Every time before loading the leaderboard, the leaderboard list will be clear. Hence, when there is no internet connection, instead of displaying the same leaderboard in the cache, it will display a blank leaderboard.

--- src/Components/Leaderboard/Leaderboard.component.js
+++ src/Components/Leaderboard/Leaderboard.component.js
@@ -117,6 +117,7 @@ const Leaderboard = ({
 
     // LEADERBOARD RANK & ADDITIONAL TICKETS RULES
     useEffect(() => {
+        setLeaderboardList([]);
         setIsGameAvailable(false);
         if (currentGameDetails?.gameId > 0) {
             dispatch(loadLeaderboardRanks(currentGameDetails?.gameId));
