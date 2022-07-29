e5395f62bb37fb1b1a573ea2de6e6ae9b3523255
Ong <yernyee@appxplore.com>
2022-07-29 17:24:44 +0800

    Change chainId to 56 temp

--- src/redux/services/index.service.js
+++ src/redux/services/index.service.js
@@ -168,7 +168,7 @@ export async function getUserWalletInfoFroyo() {
     const { data } = await axios.post(
         `${process.env.REACT_APP_FROYO_API_ENDPOINT}get-player-wallet-connect`,
         {
-            chainId: 97,
+            chainId: 56,
         },
         {
             headers: {
