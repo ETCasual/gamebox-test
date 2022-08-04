bb85688dcc4a8cbf77fdf073c059f4a7feaec8d2
Philip Peh <pehphilipk@gmail.com>
2022-08-04 16:08:42 +0800

    add loading error popups
    - comes with "showReloadAssetsPopUp " API for games to call, in order to trigger the pop ups.
    
    - Calls the game API "window.reloadAssets" to reload the game.
    
    - refactor retrySubmitModal to be more flexible

--- src/Components/Modals/RetrySubmit.modal.js
+++ src/Components/Modals/RetrySubmit.modal.js
@@ -1,10 +1,15 @@
 const RetrySubmitModal = ({
-    handleRetry,
-    handleCancel,
+    handleOk,
+    handleClose,
     disableRetry,
     title,
     subtitle,
+    okButtonText = "",
+    closeButtonText = "",
 }) => {
+    const showOkBtn = okButtonText !== "";
+    const showCloseBtn = closeButtonText !== "";
+
     return (
         <div className="container-fluid d-flex align-items-center justify-content-center modal-pop">
             <div className="modal-body-small">
@@ -12,17 +17,21 @@ const RetrySubmitModal = ({
                 <p className="subtitle pl-2">{subtitle}</p>
 
                 <div className="p-0 btn-wrapper d-flex mt-4">
-                    <button className="col btn-no" onClick={handleCancel}>
-                        SKIP
-                    </button>
-                    <button
-                        className={`col btn-yes ${
-                            !disableRetry ? "" : "opacity-0-5"
-                        }`}
-                        onClick={!disableRetry ? handleRetry : null}
-                    >
-                        RETRY
-                    </button>
+                    {showCloseBtn && (
+                        <button className="col btn-no" onClick={handleClose}>
+                            {closeButtonText}
+                        </button>
+                    )}
+                    {showOkBtn && (
+                        <button
+                            className={`col btn-yes ${
+                                !disableRetry ? "" : "opacity-0-5"
+                            }`}
+                            onClick={!disableRetry ? handleOk : null}
+                        >
+                            {okButtonText}
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
